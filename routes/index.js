5ba903cc38df66df4a47c08e94c2d91817776166
System Administrator <root@stig-fredriksens-macbook-pro.local>
2015-10-19 13:16:59 +0200

    store

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/store', function(req, res, next) {
+  res.render('store', { title: 'Store' });
+});
+
 module.exports = router;
