1f776571cb3b458bf869059bb59f46966fa7fb25
System Administrator <root@stig-fredriksens-macbook-pro.local>
2015-10-19 13:59:14 +0200

    store form

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,12 @@ router.get('/store', function(req, res, next) {
   res.render('store', { title: 'Store' });
 });
 
+router.route('/message')
+		.post(function(req,res,next){
+			console.log('received message: ' + req.body.message);
+			res.send("Message was: " + req.body.message);
+});
+
+
+
 module.exports = router;
